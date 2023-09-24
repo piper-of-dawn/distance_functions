c6defb2502146a4796249d91d68d1cce75b45157
piper_of_the_dawn <kumarshan25@gmail.com>
2023-09-24 18:45:00 +0200

    Added Maturin

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,10 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use pyo3::prelude::pyfunction;
 mod distance_functions;
 use distance_functions::levenshtein_distance::__pyo3_get_function_compute_levenshtein_distance;
 
-
-#[pyfunction]
-fn say_hello() {
-    println!("saying hello from Rust!");
-}
-
-
 #[pymodule]
 fn distance_functions(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(say_hello));
-    m.add_wrapped(wrap_pyfunction!(compute_levenshtein_distance));
+    let _ = m.add_wrapped(wrap_pyfunction!(compute_levenshtein_distance));
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-mod distance_functions;
-use distance_functions::levenshtein_distance::compute_levenshtein_distance;
-// use std::time::Instant;
-// use pyo3::prelude::{pyfunction, PyResult};
-// use pyo3::types::{PyDict, PyList};
-// use pyo3::exceptions::PyTypeError;
-// use hamming_distance::hamming_distance;
-
 fn main (){
     println!("Distance fucntions for Compare DF")
 }
